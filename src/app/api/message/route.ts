64f7a9bbfb27b67bf2957e77352c25cdd64b9c66
Abdullah-142 <abdulah14200@gmail.com>
2024-10-15 00:59:42 +0500

    first commit

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,132 +1,126 @@
-import { db } from '@/db'
-import { openai } from '@/lib/openai'
-import { getPineconeClient } from '@/lib/pinecone'
-import { SendMessageValidator } from '@/lib/validators/SendMessageValidator'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { NextRequest } from 'next/server'
+import { db } from "@/db";
+import { openai } from "@/lib/openai";
 
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
+import { NextRequest } from "next/server";
+
+import { createClient as Client } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIStream, StreamingTextResponse, GoogleGenerativeAIStream } from "ai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { Content, GenerateContentRequest, GoogleGenerativeAI, TaskType } from "@google/generative-ai";
 export const POST = async (req: NextRequest) => {
-  // endpoint for asking a question to a pdf file
+	// endpoint for asking a question to a pdf file
 
-  const body = await req.json()
+	const body = await req.json();
 
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
 
-  const { id: userId } = user
+	const { id: userId } = user;
 
-  if (!userId)
-    return new Response('Unauthorized', { status: 401 })
+	if (!userId) return new Response("Unauthorized", { status: 401 });
 
-  const { fileId, message } =
-    SendMessageValidator.parse(body)
+	const { fileId, message } = SendMessageValidator.parse(body);
 
-  const file = await db.file.findFirst({
-    where: {
-      id: fileId,
-      userId,
-    },
-  })
+	const file = await db.file.findFirst({
+		where: {
+			id: fileId,
+			userId,
+		},
+	});
 
-  if (!file)
-    return new Response('Not found', { status: 404 })
+	if (!file) return new Response("Not found", { status: 404 });
 
-  await db.message.create({
-    data: {
-      text: message,
-      isUserMessage: true,
-      userId,
-      fileId,
-    },
-  })
+	await db.message.create({
+		data: {
+			text: message,
+			isUserMessage: true,
+			userId,
+			fileId,
+		},
+	});
 
-  // 1: vectorize message
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-  })
+	const supabaseClient = Client(process.env.SUPABASE_URL!, process.env.SUPABASE_PRIVATE_KEY!);
 
-  const pinecone = await getPineconeClient()
-  const pineconeIndex = pinecone.Index('quill')
+	// 1: vectorize message
+	const embeddings = new GoogleGenerativeAIEmbeddings({
+		apiKey: process.env.OPENAI_API_KEY!,
+		model: "text-embedding-004", // 768 dimensions
+		taskType: TaskType.RETRIEVAL_DOCUMENT,
+		title: "Document title",
+	});
 
-  const vectorStore = await PineconeStore.fromExistingIndex(
-    embeddings,
-    {
-      pineconeIndex,
-      namespace: file.id,
-    }
-  )
+	const vectorStore = new SupabaseVectorStore(embeddings, {
+		client: supabaseClient,
+		tableName: "documents",
+	});
 
-  const results = await vectorStore.similaritySearch(
-    message,
-    4
-  )
+	const results = await vectorStore.similaritySearch(message, 2);
 
-  const prevMessages = await db.message.findMany({
-    where: {
-      fileId,
-    },
-    orderBy: {
-      createdAt: 'asc',
-    },
-    take: 6,
-  })
+	const prevMessages = await db.message.findMany({
+		where: {
+			fileId,
+		},
+		orderBy: {
+			createdAt: "asc",
+		},
+		take: 6,
+	});
 
-  const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage
-      ? ('user' as const)
-      : ('assistant' as const),
-    content: msg.text,
-  }))
+	const formattedPrevMessages = prevMessages.map((msg) => ({
+		role: msg.isUserMessage ? ("user" as const) : ("model" as const),
+		content: msg.text,
+	}));
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    temperature: 0,
-    stream: true,
-    messages: [
-      {
-        role: 'system',
-        content:
-          'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
-      },
-      {
-        role: 'user',
-        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-        
-  \n----------------\n
-  
-  PREVIOUS CONVERSATION:
-  ${formattedPrevMessages.map((message) => {
-    if (message.role === 'user')
-      return `User: ${message.content}\n`
-    return `Assistant: ${message.content}\n`
-  })}
-  
-  \n----------------\n
-  
-  CONTEXT:
-  ${results.map((r) => r.pageContent).join('\n\n')}
-  
-  USER INPUT: ${message}`,
-      },
-    ],
-  })
+	const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY!);
 
-  const stream = OpenAIStream(response, {
-    async onCompletion(completion) {
-      await db.message.create({
-        data: {
-          text: completion,
-          isUserMessage: false,
-          fileId,
-          userId,
-        },
-      })
-    },
-  })
+	const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-  return new StreamingTextResponse(stream)
-}
+	// Create an array of contents based on your existing context and conversation
+	const contents: Content[] = [
+		{
+			role: "model",
+			parts: [
+				{
+					text: "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+				},
+			],
+		},
+		{
+			role: "user",
+			parts: [
+				{
+					text: `Use the following pieces of context (or previous conversation if needed) to answer the user's question in markdown format.\nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+                \n----------------\n
+                
+                PREVIOUS CONVERSATION:
+                ${formattedPrevMessages
+									.map((message) => {
+										return message.role === "user" ? `User: ${message.content}\n` : `Assistant: ${message.content}\n`;
+									})
+									.join("")}
+
+                \n----------------\n
+                
+                CONTEXT:
+                ${results.map((r) => r.pageContent).join("\n\n")}
+                
+                USER INPUT: ${message}`,
+				},
+			],
+		},
+	];
+
+	// Construct the GenerateContentRequest
+	const request: GenerateContentRequest = {
+		contents: contents,
+	};
+
+	const response = await model.generateContentStream(request);
+
+	return new StreamingTextResponse(GoogleGenerativeAIStream({ stream: response.stream }));
+};
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,141 +1,130 @@
-import { db } from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import {
-  createUploadthing,
-  type FileRouter,
-} from 'uploadthing/next'
+import { db } from "@/db";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { TaskType } from "@google/generative-ai";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createClient as Client } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { getPineconeClient } from '@/lib/pinecone'
-import { getUserSubscriptionPlan } from '@/lib/stripe'
-import { PLANS } from '@/config/stripe'
-
-const f = createUploadthing()
+const f = createUploadthing();
 
 const middleware = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
 
-  if (!user || !user.id) throw new Error('Unauthorized')
+	if (!user || !user.id) throw new Error("Unauthorized");
 
-  const subscriptionPlan = await getUserSubscriptionPlan()
+	const subscriptionPlan = await getUserSubscriptionPlan();
 
-  return { subscriptionPlan, userId: user.id }
-}
+	return { subscriptionPlan, userId: user.id };
+};
 
 const onUploadComplete = async ({
-  metadata,
-  file,
+	metadata,
+	file,
 }: {
-  metadata: Awaited<ReturnType<typeof middleware>>
-  file: {
-    key: string
-    name: string
-    url: string
-  }
+	metadata: Awaited<ReturnType<typeof middleware>>;
+	file: {
+		key: string;
+		name: string;
+		url: string;
+	};
 }) => {
-  const isFileExist = await db.file.findFirst({
-    where: {
-      key: file.key,
-    },
-  })
+	const isFileExist = await db.file.findFirst({
+		where: {
+			key: file.key,
+		},
+	});
 
-  if (isFileExist) return
+	if (isFileExist) return;
 
-  const createdFile = await db.file.create({
-    data: {
-      key: file.key,
-      name: file.name,
-      userId: metadata.userId,
-      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      uploadStatus: 'PROCESSING',
-    },
-  })
+	const createdFile = await db.file.create({
+		data: {
+			key: file.key,
+			name: file.name,
+			userId: metadata.userId,
+			url: file.url,
+			uploadStatus: "PROCESSING",
+		},
+	});
 
-  try {
-    const response = await fetch(
-      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-    )
+	try {
+		const response = await fetch(file.url);
 
-    const blob = await response.blob()
+		const blob = await response.blob();
 
-    const loader = new PDFLoader(blob)
+		const loader = new PDFLoader(blob);
 
-    const pageLevelDocs = await loader.load()
+		const pageLevelDocs = await loader.load();
 
-    const pagesAmt = pageLevelDocs.length
+		const pagesAmt = pageLevelDocs.length;
 
-    const { subscriptionPlan } = metadata
-    const { isSubscribed } = subscriptionPlan
+		// const { subscriptionPlan } = metadata;
+		// const { isSubscribed } = subscriptionPlan;
 
-    const isProExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
-    const isFreeExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Free')!
-        .pagesPerPdf
+		// const isProExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+		// const isFreeExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
 
-    if (
-      (isSubscribed && isProExceeded) ||
-      (!isSubscribed && isFreeExceeded)
-    ) {
-      await db.file.update({
-        data: {
-          uploadStatus: 'FAILED',
-        },
-        where: {
-          id: createdFile.id,
-        },
-      })
-    }
+		// if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+		// 	await db.file.update({
+		// 		data: {
+		// 			uploadStatus: "FAILED",
+		// 		},
+		// 		where: {
+		// 			id: createdFile.id,
+		// 		},
+		// 	});
+		// }
 
-    // vectorize and index entire document
-    const pinecone = await getPineconeClient()
-    const pineconeIndex = pinecone.Index('quill')
+		// vectorize and index entire document
 
-    const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY,
-    })
+		const supabaseClient = Client(process.env.SUPABASE_URL!, process.env.SUPABASE_PRIVATE_KEY!);
 
-    await PineconeStore.fromDocuments(
-      pageLevelDocs,
-      embeddings,
-      {
-        pineconeIndex,
-        namespace: createdFile.id,
-      }
-    )
+		const embeddings = new GoogleGenerativeAIEmbeddings({
+			apiKey: process.env.OPENAI_API_KEY!,
+			model: "text-embedding-004", // 768 dimensions
+			taskType: TaskType.RETRIEVAL_DOCUMENT,
+			title: "Document title",
+		});
 
-    await db.file.update({
-      data: {
-        uploadStatus: 'SUCCESS',
-      },
-      where: {
-        id: createdFile.id,
-      },
-    })
-  } catch (err) {
-    await db.file.update({
-      data: {
-        uploadStatus: 'FAILED',
-      },
-      where: {
-        id: createdFile.id,
-      },
-    })
-  }
-}
+		const vectorStore = await SupabaseVectorStore.fromDocuments(pageLevelDocs, embeddings, {
+			client: supabaseClient,
+			tableName: "documents",
+		});
+
+		await db.file.update({
+			data: {
+				uploadStatus: "SUCCESS",
+			},
+			where: {
+				id: createdFile.id,
+			},
+		});
+	} catch (err) {
+		await db.file.update({
+			data: {
+				uploadStatus: "FAILED",
+			},
+			where: {
+				id: createdFile.id,
+			},
+		});
+		console.log(err);
+	}
+};
 
 export const ourFileRouter = {
-  freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
-    .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
-  proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
-    .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
-} satisfies FileRouter
+	freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+		.middleware(middleware)
+		.onUploadComplete(onUploadComplete),
+	proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+		.middleware(middleware)
+		.onUploadComplete(onUploadComplete),
+} satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter
+export type OurFileRouter = typeof ourFileRouter;
+function createClient(SUPABASE_URL: string | undefined, SUPABASE_PRIVATE_KEY: string | undefined) {
+	throw new Error("Function not implemented.");
+}
